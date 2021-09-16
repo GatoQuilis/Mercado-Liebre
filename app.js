c204b907ad54e9c1ce52e1ce3e2e939600642217
GatoQuilis <franko_222@hotmail.com>
2021-09-15 21:04:32 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const path = require ('path');
+
+const morgan = require ('morgan');
+
+const app = express();
+
+const port = 3000;
+
+app.use (express.static('public'));
+
+app.listen (port, () => console.log('Server up at: http://localhost:3000'));
+
+app.get ('/', (req, res) => {
+    res.sendFile (path.resolve ('./views/home.html'));
+})
+
