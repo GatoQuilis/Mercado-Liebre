ecced7ac3064caedde7150d8ff5d82bbb993b28a
GatoQuilis <franko_222@hotmail.com>
2021-09-16 22:45:17 -0300

    proctife 2

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const morgan = require ('morgan');
 
 const app = express();
 
-const PORT = process.env.port || 3000;
+const port = process.env.port || 3000;
 
 app.use (express.static('public'));
 
-app.listen (PORT, () => console.log('Server up at: http://localhost:3000'));
+app.listen (port, () => console.log('Server up at: http://localhost:3000'));
 
 app.get ('/', (req, res) => {
     res.sendFile (path.resolve ('./views/home.html'));
